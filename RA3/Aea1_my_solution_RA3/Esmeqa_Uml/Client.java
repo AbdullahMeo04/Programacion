e3d11591bb20ba7c8ea1de6d5c5595956dc327a8
MEO <abab@pratfp.com>
2026-02-04 20:50:25 +0100

    updated

--- RA3/Aea1_my_solution_RA3/Esmeqa_Uml/Client.java
+++ RA3/Aea1_my_solution_RA3/Esmeqa_Uml/Client.java
@@ -4,6 +4,24 @@ public class Client {
     private String telefono;
     private String e_mail;
 
+    public Client(String nom, String adreca, String telefon, String e_mail){
+        this.nom = nom;
+        this.adreca = adreca;
+        this.telefon = telefon;
+        this.e_mail = e_mail;    
+    }
+    public String getnom(){return nom;}
+    public void setnom(String nom) {this.nom = nom;}
+    
+    public String getadreca(){return adreca;}
+    public void setadreca(String adreca) {this.adreca = adreca;}
+    
+    public String gettelefon(){return telefon;}
+    public void settelefon(String telefon) {this.telefon = telefon;}
+    
+    public String gete_mail(){return e_mail;}
+    public void sete_mail(String e_mail) {this.e_mail = e_mail;}
+
     public void assingnarEncarrec(Encarrec e){}
     public void anullarEncarrec(Encarrec e){}
 }
