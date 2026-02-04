da6f29193f0a1c8c1be10375ef8c005f4e26bf2c
MEO <abab@pratfp.com>
2026-02-04 20:55:40 +0100

    updated

--- RA3/Aea1_my_solution_RA3/Esmeqa_Uml/TipusProducte.java
+++ RA3/Aea1_my_solution_RA3/Esmeqa_Uml/TipusProducte.java
@@ -4,4 +4,27 @@ public class TipusProducte {
     private double preu;
     private int estoc;
     private boolean aLaVenda;
+
+    public TipusProducte(String nom, int codiIdentificardor, double preu, int estoc, boolean aLaVenda) {
+        this.nom = nom;
+        this.codiIdentificardor = codiIdentificardor;
+        this.preu = preu;
+        this.estoc = estoc;
+        this.aLaVenda = aLaVenda;
+    }
+
+    public String getnom() { return nom; }
+    public void setnom(String nom) { this.nom = nom; }
+
+    public int getcodiIdentificardor() { return codiIdentificardor; }
+    public void setcodiIdentificardor(int codiIdentificardor) { this.codiIdentificardor = codiIdentificardor; }
+
+    public double getpreu() { return preu; }
+    public void setpreu(double preu) { this.preu = preu; }
+
+    public int getestoc() { return estoc; }
+    public void setestoc(int estoc) { this.estoc = estoc; }
+
+    public boolean getaLaVenda() { return aLaVenda; }
+    public void setaLaVenda(boolean aLaVenda) { this.aLaVenda = aLaVenda; }
 }
