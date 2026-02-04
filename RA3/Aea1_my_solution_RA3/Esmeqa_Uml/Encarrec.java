cf2b818124c7f862479865e1cfb8b37474ee9c21
MEO <abab@pratfp.com>
2026-02-04 20:54:27 +0100

    updated

--- RA3/Aea1_my_solution_RA3/Esmeqa_Uml/Encarrec.java
+++ RA3/Aea1_my_solution_RA3/Esmeqa_Uml/Encarrec.java
@@ -4,6 +4,25 @@ public class Encarrec {
     private int hora;
     private int minuts;
 
-    public void afegirProducte(Peticio p){}
-    public void eliminarProducte(Peticio p){}
+    public Encarrec(int dia, int mes, int hora, int minuts) {
+        this.dia = dia;
+        this.mes = mes;
+        this.hora = hora;
+        this.minuts = minuts;
+    }
+
+    public int getdia() { return dia; }
+    public void setdia(int dia) { this.dia = dia; }
+
+    public int getmes() { return mes; }
+    public void setmes(int mes) { this.mes = mes; }
+
+    public int gethora() { return hora; }
+    public void sethora(int hora) { this.hora = hora; }
+
+    public int getminuts() { return minuts; }
+    public void setminuts(int minuts) { this.minuts = minuts; }
+
+    public void afegirProducte(Peticio p) {}
+    public void eliminarProducte(Peticio p) {}
 }
