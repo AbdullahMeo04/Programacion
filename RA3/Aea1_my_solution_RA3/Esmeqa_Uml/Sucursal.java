6ec1512f740231c8aa58375b6aa844b9805c749f
MEO <abab@pratfp.com>
2026-02-04 18:04:29 +0100

    update

--- RA3/Aea1_my_solution_RA3/Esmeqa_Uml/Sucursal.java
+++ RA3/Aea1_my_solution_RA3/Esmeqa_Uml/Sucursal.java
@@ -13,15 +13,27 @@ public Sucursal(String nom, String adreca, String telefon, String e_mail){
 public String getnom(){
     return nom;
 }
+    public void setname(String nom){
+        this.nom = nom;
+    }
 public String getadreca(){
     return adreca;
 }
+    public void setadreca(string adreca){
+        this.adreca = adreca;
+    }
 public String gettelefon(){
     return telefon;
 }
+    public void settelefon(string telefon){
+        this.telefon = telefon;
+    }
 public String getne_mail(){
     return e_mail;
 }
+    public void sete_mail(string e_mail){
+        this.e_mail = e_mail;
+    }
     public void descansa(Transportista t){}
     public void treballa(Transportista t){}
     public void altaClient(Client c){}
