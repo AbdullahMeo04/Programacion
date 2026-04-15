45a23e7c740c280408ee24d767be5998e1740dec
MEO <abab@pratfp.com>
2026-04-15 23:59:43 +0200

    "new comments"

---
+++ RA4/file_reading/file_reader/File_reader1.java
@@ -0,0 +1,39 @@
+package RA4.file_reading;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class InvertFile {
+    public static void main(String[] args) {
+
+        File inputFile = new File("arxiu.txt");
+
+        try {
+
+            if (!inputFile.exists()) {
+                System.out.println("File arxiu.txt not found!");
+                return;
+            }
+
+            Scanner scn = new Scanner(inputFile);
+            FileWriter writer = new FileWriter("arxiu_invertit.txt");
+
+            while (scn.hasNextLine()) {
+                String line = scn.nextLine();
+                String reversed = new StringBuilder(line).reverse().toString();
+                writer.write(reversed + "\n");
+            }
+            scn.close();
+            writer.close();
+
+            System.out.println("File successfully processed!");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: file not found.");
+        } catch (IOException e) {
+            System.out.println("Error writing file.");
+        }
+    }
+}
