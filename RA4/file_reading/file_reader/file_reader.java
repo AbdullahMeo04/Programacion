32c210ecd3dca4d9e2acc52b673f69bbec9cc0f0
MEO <abab@pratfp.com>
2026-04-19 11:58:49 +0200

    "last commit"

--- RA4/file_reading/file_reader/file_reader.java
+++ RA4/file_reading/file_reader/file_reader.java
@@ -1,4 +1,4 @@
-package RA4.file_reading;
+package RA4.file_reading.file_reader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class InvertFile2 {
+public class file_reading {
     public static void main(String[] args) {
 
         File inputFile = new File("arxiu.txt");
@@ -20,20 +20,15 @@ public class InvertFile2 {
 
             Scanner scn = new Scanner(inputFile);
             FileWriter writer = new FileWriter("arxiu_invertit.txt");
-
             while (scn.hasNextLine()) {
-
                 String line = scn.nextLine();
                 String reversed = new StringBuilder(line).reverse().toString();
-
                 writer.write(reversed + "\n");
             }
-
             scn.close();
             writer.close();
 
             System.out.println("File successfully processed!");
-
         } catch (FileNotFoundException e) {
             System.out.println("Error: file not found.");
         } catch (IOException e) {
---
+++ RA4/file_reading/file_reader2/WordCounter.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        int count = readFileAndCount("arxiu.txt");
+
+        if (count != -1) {
+            writeFile("sortida.txt", count);
+        }
+    }
+
+    public static int readFileAndCount(String fileName) {
+        int words = 0;
+        File file = new File(fileName);
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                scanner.next();
+                words++;
+            }
+            return words;
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: arxiu.txt not found.");
+            return -1;
+        }
+    }
+
+    public static void writeFile(String fileName, int count) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, false))) {
+            writer.println("Total words: " + count);
+            System.out.println("Process finished successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred during writing.");
+        }
+    }
+}
